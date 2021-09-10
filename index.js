e2ea5b97f52aee4e8942f2c473422f9c104546cb
Will Knowles <will@willknowles.com>
2021-09-10 17:00:47 -0500

    console.table, jest, and pseudocoded

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
 /*
 
+POA:
+
+1. Create Github Repo and Clone to Local
+2. Node: npm init
+3. Modules: npm install inquirer mysql2 console.table jest
+4. Pseudo-Code *
+    - data layer: schema and seed files
+    - base classes, tests, and query methods
+    - inquirer scripts
+
+Classes:
+EmployeeTracker - app wrapper - start, action-menu, action-handler
+Employee
+Department
+Role
+
+
+start
+menu: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+-all departments: formatted table, department.name and department.id
+-all roles: job_title, role.id, role.department, role.salary
+-all employees: formatted table, employee.id, employee.first_name, employee.last_name, job_titles, departments, salaries, and employee.managers
+-add department: prompt to enter department name
+-add role: prompt to enter name, salary, and department
+-add employee: prompt to enter first name, last name, role, and manager
+-update employee role: prompt to select employee and update role
+-update employee department: prompt to select employee and update department *bonus*
+-view employees by manager *bonus*
+-view employees by department *bonus*
+-delete department, role, and employee *bonus*
+-view department by cumulative salary *bonus*
+
+
+AS A business owner
+I WANT to be able to view and manage the departments, roles, and employees in my company
+SO THAT I can organize and plan my business
+
+GIVEN a command-line application that accepts user input
+WHEN I start the application
+THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+WHEN I choose to view all departments
+THEN I am presented with a formatted table showing department names and department ids
+WHEN I choose to view all roles
+THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+WHEN I choose to view all employees
+THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+WHEN I choose to add a department
+THEN I am prompted to enter the name of the department and that department is added to the database
+WHEN I choose to add a role
+THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+WHEN I choose to add an employee
+THEN I am prompted to enter the employee’s first name, last name, role, and manager and that employee is added to the database
+WHEN I choose to update an employee role
+THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+
+asynchronous queries: https://www.npmjs.com/package/mysql2
+
+BONUS: 
+- Update employee managers.
+- View employees by manager.
+- View employees by department.
+- Delete departments, roles, and employees.
+- View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
+
 */
