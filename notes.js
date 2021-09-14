eea642a8ecb4bee59b6615af5428d34ce06f5d7b
Will Knowles <will@willknowles.com>
2021-09-13 19:55:44 -0500

    view department salaries complete

--- notes.js
+++ notes.js
@@ -77,7 +77,7 @@ BONUS:
 * Update employee managers.
 * View employees by manager.
 * View employees by department.
-- Delete departments, roles, and employees.
-- View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
+* Delete departments, roles, and employees.
+* View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
 
 */
